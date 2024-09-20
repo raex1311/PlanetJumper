47870e8142e42678ba25d8faf1efa742c27d5ea3
raex1311 <78586870+raex1311@users.noreply.github.com>
2024-09-20 23:57:11 +0530

    working properly after adding shader

--- Scripts/Procedural Generation/planetshader/planetshader.gd
+++ Scripts/Procedural Generation/planetshader/planetshader.gd
@@ -8,9 +8,12 @@ var val = 10.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	mat = self.get_material()
-	print("Shader Material!!", mat.resource_name)
 	mat.set_shader_parameter("seed",randi() % 10000)
 
+func randomize():
+	print("Randomizing Colors of Planet!!")
+	mat = self.get_material()
+	mat.set_shader_parameter("seed",randi() % 10000)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	rotate(0.11*delta)
